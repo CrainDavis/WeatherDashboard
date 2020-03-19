544f10aa4e71ce7eab577aae16ba5882cade42e8
Chyna Davis <chyna.davis11@gmail.com>
2020-03-19 12:29:53 -0700

    remove Pexels.com API call

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## Project Description:
 * This project makes use of several Application Programing Interfaces (APIs) to create a dynamic user-friendly application. The weather dashboard, by employing information provided by the OpenWeatherAPI, first presents the user with a search bar, with the instructions to input the name of a city. Upon clicking the search button, the user will then be able to see current weather data for the location that they input. In the Current Weather Data section, the user will be given the following information: the city name (that they chose), the current date (via the Moment.js library), a weather icon indicating the current weather conditions, and written information for the weather conditions, temperature, humidity, wind speed, and UV index. Furthermore, the UV index also indicates through the use of color how severe it is (with green being the safest, and red indicating the most dangerous). 
 * The user is also able to view a 5-day forecast for the city of their choice. This forecast displays the noon-time temperature and humidity information for the following five days. 
-* In addition, the application also makes use of the Pexels.com API, a site that allows people to pull images from the site. This Weather Dashboard application uses Pexels.com's API to grab images based on the user's input city name. If the user were to type in "Los Angeles", the image will be one from this city. In the case that Pexels API does not possess any images of a particular city, the application is coded to instead display an image from the "images" folder. This image is chosen based on the OpenWeather API's weather description (ie "clear sky", "broken clouds", "heavy snow", etc.). 
+* In addition, the application is coded to display an image from the "images" folder. All images are originally from Pexels.com. This image displayed in the application is chosen based on the OpenWeather API's weather description (ie "clear sky", "broken clouds", "heavy snow", etc.) for the user's chosen city. Links to Pexels.com and credits to the photograph are displayed below the image.
 * Lastly, this application also saves the user's previous searches in a list, which also function as buttons for the user to click and check again. This function is made possible through employing the browser's local storage capability. Through this local storage, the user can also view their most-recently searched city upon re-opening the application. The user also has the choice to clear their search history if they wish, simply by clicking the clear-all button.
 ## Image of Application:
 `![Image description](link-to-image)`
@@ -12,8 +12,8 @@
     * current weather data (https://openweathermap.org/current)
     * 5 day weather forecast (https://openweathermap.org/forecast5)
     * ultraviolet index (beta) (https://openweathermap.org/api/uvi)
-* Pexels API (https://www.pexels.com/)
-    * documentation (https://www.pexels.com/api/documentation/)
+* Pexels.com (https://www.pexels.com/)
+    * see application for credits for each individual image
 * Moment.js library (https://momentjs.com/)
 ## URLs:
 * deployed application: https://craindavis.github.io/WeatherDashboard/
--- assets/script.js
+++ assets/script.js
@@ -34,101 +34,53 @@ function displayWeatherInfo() {
           $("#currentHumidity").text(mainResponse.main.humidity + "%");
           $("#currentWind").text(mainResponse.wind.speed + " mph");
 
-          // insert an image via
+          // insert an image from Pexels.com
           function weatherImage() {
-            var pexelsAPIkey = "563492ad6f91700001000001e15517b4e1ce413ab4eae6a9eac1a519";
-
-            // AJAX call for Pexels.com API
-            $.ajax({
-              url: "https://api.pexels.com/v1/search?query=" + searchedCity + "&per_page=15&page=1",
-              method: "GET",
-              // enter authorization key for Pexels.com's API
-              beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", pexelsAPIkey);
-              }
-            }).then(function(imageResponse) {
-              console.log(imageResponse);
-
-              // prevent blank image from displaying if Pexels.com's API has no image in store
-              if (imageResponse.total_results === 0) {
-                $("#weatherImage").addClass("hide");
-                $("#imageLink").addClass("hide");
-                $("#imageSource").addClass("hide");
-                // remove "hide" class from weather info sections
-                $("#currentWeatherSection").removeClass("hide");
-                $("#weatherImage").removeClass("hide");
-                $("#imageLink").removeClass("hide");
-                $("#imageSource").removeClass("hide");
-                $("#forecastWeatherSection").removeClass("hide");
-
-                // display an image from the images folder based on OpenWeather API's weather description
-                if (mainResponse.weather[0].description === "clear sky") {
-                  $("#weatherImage").attr("src", "assets/images/sky-clear.jpeg");
-                  $("#imageSource").text("photo by: Francesco Ungaro").attr("href", "https://www.pexels.com/@francesco-ungaro");
-                } else if (mainResponse.weather[0].description === "few clouds") {
-                  $("#weatherImage").attr("src", "assets/images/clouds-few.jpeg");
-                  $("#imageSource").text("photo by: Pixabay").attr("href", "https://www.pexels.com/@pixabay");
-                } else if (mainResponse.weather[0].description === "scattered clouds") {
-                  $("#weatherImage").attr("src", "assets/images/clouds-scattered.jpeg");
-                  $("#imageSource").text("photo by: Pixabay").attr("href", "https://www.pexels.com/@pixabay");
-                } else if (mainResponse.weather[0].description === "broken clouds") {
-                  $("#weatherImage").attr("src", "assets/images/clouds-broken.jpeg");
-                  $("#imageSource").text("photo by: Brett Sayles");
-                  $("#imageSource").attr("href", "https://www.pexels.com/@brett-sayles");
-                } else if (mainResponse.weather[0].description === "overcast clouds") {
-                  $("#weatherImage").attr("src","assets/images/clouds-overcast.jpeg");
-                  $("#imageSource").text("photo by: Ben Herbert").attr("href", "https://www.pexels.com/@nzbenzimages");
-                } else if (mainResponse.weather[0].description === "light rain") {
-                  $("#weatherImage").attr("src", "assets/images/rain-light.jpeg");
-                  $("#imageSource").text("photo by: Kaique Rocha").attr("href", "https://www.pexels.com/@kaiquestr");
-                } else if (mainResponse.weather[0].description === "moderate rain") {
-                  $("#weatherImage").attr("src", "assets/images/rain-moderate.jpeg");
-                  $("#imageSource").text("photo by: Bedis ElAcheche").attr("href", "https://www.pexels.com/@bedis-elacheche-310026");
-                } else if (mainResponse.weather[0].description === "heavy intensity rain") {
-                  $("#weatherImage").attr("src", "assets/images/rain-heavy.jpeg");
-                  $("#imageSource").text("photo by: Aleksandar Pasaric").attr("href", "https://www.pexels.com/@apasaric");
-                } else if (mainResponse.weather[0].description === "light snow") {
-                  $("#weatherImage").attr("src", "assets/images/snow-light.jpeg");
-                  $("#imageSource").text("photo by: Oleg Magni").attr("href", "https://www.pexels.com/@oleg-magni");
-                } else if (mainResponse.weather[0].description === "snow") {
-                  $("#weatherImage").attr("src", "assets/images/snow-moderate.jpeg");
-                  $("#imageSource").text("photo by: Gratisography").attr("href","https://www.pexels.com/@gratisography");
-                } else if (mainResponse.weather[0].description === "heavy snow") {
-                  $("#weatherImage").attr("src", "assets/images/snow-heavy.jpeg");
-                  $("#imageSource").text("photo by: Pixabay").attr("href", "https://www.pexels.com/@pixabay");
-                } else {
-                  $("#weatherImage").addClass("hide");
-                  $("#imageLink").addClass("hide");
-                  $("#imageSource").addClass("hide");
-                }
-
-                // otherwise, display retrieve image/links from Pexels API
-              } else if (imageResponse.total_results != 0) {
-                // remove "hide" class from weather info sections (once user enters in a city name)
-                $("#currentWeatherSection").removeClass("hide");
-                $("#weatherImage").removeClass("hide");
-                $("#forecastWeatherSection").removeClass("hide");
-                $("#imageLink").removeClass("hide");
-                $("#imageSource").removeClass("hide");
-
-                function getRandomIndex() {
-                  // empty out previous randomized number submissions
-                  $("#weatherImage").empty();
-                  $("#imageSource").empty();
-
-                  // fill in image and text with a (new) random selection from the photos array
-                  var randomIndex =
-                    imageResponse.photos[
-                      Math.floor(Math.random() * imageResponse.photos.length)
-                    ];
-                  $("#weatherImage").attr("src", randomIndex.src.landscape);
-                  $("#imageSource").text("photo by: " + randomIndex.photographer);
-                  $("#imageSource").attr("href", randomIndex.photographer_url);
-                }
-                // call function
-                getRandomIndex();
-              }
-            });
+            $("#currentWeatherSection").removeClass("hide");
+            $("#weatherImage").removeClass("hide");
+            $("#imageLink").removeClass("hide");
+            $("#imageSource").removeClass("hide");
+            $("#forecastWeatherSection").removeClass("hide");
+            // display an image from the images folder based on OpenWeather API's weather description
+            if (mainResponse.weather[0].description === "clear sky") {
+              $("#weatherImage").attr("src", "assets/images/sky-clear.jpeg");
+              $("#imageSource").text("photo by: Francesco Ungaro").attr("href", "https://www.pexels.com/@francesco-ungaro");
+            } else if (mainResponse.weather[0].description === "few clouds") {
+              $("#weatherImage").attr("src", "assets/images/clouds-few.jpeg");
+              $("#imageSource").text("photo by: Pixabay").attr("href", "https://www.pexels.com/@pixabay");
+            } else if (mainResponse.weather[0].description === "scattered clouds") {
+              $("#weatherImage").attr("src", "assets/images/clouds-scattered.jpeg");
+              $("#imageSource").text("photo by: Pixabay").attr("href", "https://www.pexels.com/@pixabay");
+            } else if (mainResponse.weather[0].description === "broken clouds") {
+              $("#weatherImage").attr("src", "assets/images/clouds-broken.jpeg");
+              $("#imageSource").text("photo by: Brett Sayles");
+              $("#imageSource").attr("href", "https://www.pexels.com/@brett-sayles");
+            } else if (mainResponse.weather[0].description === "overcast clouds") {
+              $("#weatherImage").attr("src","assets/images/clouds-overcast.jpeg");
+              $("#imageSource").text("photo by: Ben Herbert").attr("href", "https://www.pexels.com/@nzbenzimages");
+            } else if (mainResponse.weather[0].description === "light rain") {
+              $("#weatherImage").attr("src", "assets/images/rain-light.jpeg");
+              $("#imageSource").text("photo by: Kaique Rocha").attr("href", "https://www.pexels.com/@kaiquestr");
+            } else if (mainResponse.weather[0].description === "moderate rain") {
+              $("#weatherImage").attr("src", "assets/images/rain-moderate.jpeg");
+              $("#imageSource").text("photo by: Bedis ElAcheche").attr("href", "https://www.pexels.com/@bedis-elacheche-310026");
+            } else if (mainResponse.weather[0].description === "heavy intensity rain") {
+              $("#weatherImage").attr("src", "assets/images/rain-heavy.jpeg");
+              $("#imageSource").text("photo by: Aleksandar Pasaric").attr("href", "https://www.pexels.com/@apasaric");
+            } else if (mainResponse.weather[0].description === "light snow") {
+              $("#weatherImage").attr("src", "assets/images/snow-light.jpeg");
+              $("#imageSource").text("photo by: Oleg Magni").attr("href", "https://www.pexels.com/@oleg-magni");
+            } else if (mainResponse.weather[0].description === "snow") {
+              $("#weatherImage").attr("src", "assets/images/snow-moderate.jpeg");
+              $("#imageSource").text("photo by: Gratisography").attr("href","https://www.pexels.com/@gratisography");
+            } else if (mainResponse.weather[0].description === "heavy snow") {
+              $("#weatherImage").attr("src", "assets/images/snow-heavy.jpeg");
+              $("#imageSource").text("photo by: Pixabay").attr("href", "https://www.pexels.com/@pixabay");
+            } else {
+              $("#weatherImage").addClass("hide");
+              $("#imageLink").addClass("hide");
+              $("#imageSource").addClass("hide");
+            }
           }
 
           // call function
